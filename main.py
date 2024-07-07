2a34e6131ef63dd8a736bd55514e2a5b3d9934d6
Harsha <harshahm0130@gmail.com>
2024-07-07 11:08:48 +0530

    Creating graph with configuration and header for hiding token

--- main.py
+++ main.py
@@ -1,14 +1,32 @@
 import requests
+TOKEN = "aebe924sdvadsom"
+USERNAME = "harshahmm"
 
 pixela_endpoint = "https://pixe.la/v1/users"
 
 user_params = {
-    "token": "aebe924sdvadsom",
-    "username": "harshahmm",
+    "token": TOKEN,
+    "username": USERNAME,
     "agreeTermsOfService": "yes",
     "notMinor": "yes",
-    "thanksCode": "Thank you!",
 }
 
-response = requests.post(url=pixela_endpoint, json=user_params)
+# response = requests.post(url=pixela_endpoint, json=user_params)
+# print(response.text)
+
+graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
+
+header = {
+    "X-USER-TOKEN": TOKEN
+}
+graph_config = {
+    "id": "graph1",
+    "name": "Running Graph",
+    "unit": "Km",
+    "type": "float",
+    "color": "ajisai"
+}
+
+response = requests.post(url=graph_endpoint, json=graph_config, headers=header)
 print(response.text)
+
